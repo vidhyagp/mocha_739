6257f81ebed78a726b7762df8c226d5fc21e7d5d
Sivasubramanian.R <sivas@cs.wisc.edu>
2013-11-12 17:35:07 -0600

    Auth_basic_app

---
+++ mocha/mocha/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'mocha.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    #url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/', 'mocha_models.views.loginview'),
+    url(r'^auth/', 'mocha_models.views.auth_and_login'),
+    url(r'^signup/', 'mocha_models.views.sign_up_in'),
+    url(r'^$', 'mocha_models.views.secured'),
+)
