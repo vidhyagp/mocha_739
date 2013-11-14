86ec15271ae3b9a21e01a3cd0e70b2c2d85845c1
Sivasubramanian.R <sivas@cs.wisc.edu>
2013-11-13 23:05:02 -0600

    working version. basic functionality using mysql tables done.

--- mocha/mocha_models/forms.py
+++ mocha/mocha_models/forms.py
@@ -5,10 +5,12 @@ class TopicContentForm(forms.ModelForm):
     class Meta:
         model = TopicContent
         fields = ['topic', 'content', 'userid']
+	exclude = ['userid']
 
 class UserTopicForm(forms.ModelForm):
     class Meta:
         model = UserTopic
         fields = ['userid', 'topic']
+	exclude = ['userid']
 
 
--- mocha/mocha_models/views.py
+++ mocha/mocha_models/views.py
@@ -7,6 +7,9 @@ from django.http import HttpResponseRedirect
 from mocha_models.models import *
 from mocha_models.forms import *
 from django.views.generic.edit import FormView
+from django.db import connections, transaction
+from django.http import Http404
+from django.db import connection
 
 def loginview(request):
     c = {}
@@ -41,23 +44,74 @@ def sign_up_in(request):
     else:
     	return redirect("/login/")
 
-def tabs(request):
-     return render_to_response('index.html')
+#def tabs(request):
+#     return render_to_response('index.html')
 
+@login_required(login_url='/login/')
 def create_topic(request):
     if request.method == 'POST': # If the form has been submitted...
         form = TopicContentForm(request.POST) # A form bound to the POST data
-        #if form.is_valid(): # All validation rules pass
-            # Process the data in form.cleaned_data
-            # ...
-            #return HttpResponseRedirect('/thanks/') # Redirect after POST
+	obj = form.save(commit=False)
+	obj.userid = request.user
+	obj.save()
+	return redirect("/home/")
     else:
         form = TopicContentForm() # An unbound form
-
-    return render(request, 'createTopic.html', {
-        'form': form,
-    })
+	return render(request, 'createTopic.html', {'form': form,})
 
 @login_required(login_url='/login/')
-def secured(request):
-    return render_to_response("index.html")
+def subscribe_topic(request):
+    if request.method == 'POST': # If the form has been submitted...
+        form = UserTopicForm(request.POST) # A form bound to the POST data
+	obj = form.save(commit=False)
+	obj.userid = request.user
+	obj.save()
+	return redirect("/home/")
+    else:
+        form = UserTopicForm() # An unbound form
+	return render(request, 'subscribeTopic.html', {'form': form,})
+
+    
+#@login_required(login_url='/login/')
+#def secured(request):
+#    return render_to_response("display.html")
+
+
+DBNAME = 'default'
+#allll results of a SQL as a list of dicts
+def dbfetchall(sql, params=[]):
+    #"Returns all rows from SQL query as a dict"
+    global DBNAME
+    cur = connections[DBNAME].cursor()
+    cur.execute(sql, params)
+    desc = cur.description
+    return [
+        dict(zip([col[0] for col in desc], row))
+        for row in cur.fetchall()
+    ]
+
+def dbfetchcontent(topic):
+   rows = dbfetchall('select content from topic_content where topic = \'' + str(topic) + '\' ORDER BY timestamp') 
+   return [c['content'] for c in rows] 
+
+
+def dbfetchtopics(username):
+   rows = dbfetchall('select topic from user_topic where userid = \'' + str(username) + '\' ORDER BY timestamp') 
+   return [c['topic'] for c in rows] 
+
+
+@login_required(login_url='/login/')
+def render_topics(request):
+   username = request.user
+   topics = dbfetchtopics(username)
+   print topics
+   return render(request, 'display.html', { 'topics' : topics })
+
+
+@login_required(login_url='/login/')
+def render_content(request, topic):
+   content = dbfetchcontent(topic)
+   return render(request, 'displayContent.html', { 'topic' : topic , 'content' : content })
+
+
+
